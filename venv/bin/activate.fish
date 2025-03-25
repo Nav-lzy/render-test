9a93b1577afa4c9102251443ac66376cc112609a
Nav <stu241725@mail.uni-kiel.de>
2025-03-25 11:28:05 +0100

    Removed unecessary dependencies from requirement.txt

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/nav/DASH_Tutorial/venv
+set -gx VIRTUAL_ENV /home/nav/DASH_Tutorial/render-test/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
